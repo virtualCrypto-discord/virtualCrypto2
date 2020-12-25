b88b0f813c8b16c78da05916bf1a92b23f2d21ad
Sumito Izumita <sumito@izumita.com>
2020-12-25 15:18:08 +0900

    add view
    各コマンドのviewの作成

--- lib/virtualCrypto_web/views/interactions_view.ex
+++ lib/virtualCrypto_web/views/interactions_view.ex
@@ -7,7 +7,79 @@ defmodule VirtualCryptoWeb.InteractionsView do
     }
   end
 
-  def render( "interactions.json", %{ params: params } ) do
+  def render( "bal.json", %{ params: params } ) do
+    %{
+      type: 4,
+      data: %{
+        tts: false,
+        content: "Congrats on sending your command!",
+        embeds: [],
+        allowed_mentions: []
+      }
+    }
+  end
+
+  def render( "pay.json", %{ params: params } ) do
+    %{
+      type: 4,
+      data: %{
+        tts: false,
+        content: "Congrats on sending your command!",
+        embeds: [],
+        allowed_mentions: []
+      }
+    }
+  end
+
+  def render( "give.json", %{ params: params } ) do
+    %{
+      type: 4,
+      data: %{
+        tts: false,
+        content: "Congrats on sending your command!",
+        embeds: [],
+        allowed_mentions: []
+      }
+    }
+  end
+
+  def render( "create.json", %{ params: params } ) do
+    %{
+      type: 4,
+      data: %{
+        tts: false,
+        content: "Congrats on sending your command!",
+        embeds: [],
+        allowed_mentions: []
+      }
+    }
+  end
+
+  def render( "info.json", %{ params: params } ) do
+    %{
+      type: 4,
+      data: %{
+        tts: false,
+        content: "Congrats on sending your command!",
+        embeds: [],
+        allowed_mentions: []
+      }
+    }
+  end
+
+  def render( "help.json", %{ params: params } ) do
+    %{
+      type: 4,
+      data: %{
+        tts: false,
+        content: "Congrats on sending your command!",
+        embeds: [],
+        allowed_mentions: []
+      }
+    }
+  end
+
+  def render( "invite.json", %{ params: params } ) do
     %{
       type: 4,
       data: %{
