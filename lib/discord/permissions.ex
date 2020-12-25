46b0ab9e43ab5f25adfb700ab727010d01f5a66e
Sumito Izumita <sumito@izumita.com>
2020-12-25 21:36:03 +0900

    add permission check

---
+++ lib/discord/permissions.ex
@@ -0,0 +1,12 @@
+defmodule Discord.Permissions do
+  @moduledoc false
+  use Bitwise
+
+  @spec check(integer, integer) :: bool
+  def check(permissions, permission) do
+    (permissions &&& permission) == permission
+  end
+
+  def administrator(), do: 0x8
+
+end
--- lib/virtualCrypto_web/command_handler.ex
+++ lib/virtualCrypto_web/command_handler.ex
@@ -23,17 +23,22 @@ defmodule VirtualCryptoWeb.CommandHandler do
   end
 
   def handle("create", options, %{ "guild_id" => guild_id } = params) do
-    {int_guild_id, _} = Integer.parse(guild_id)
-    if name_unit_check(options["name"], options["unit"]) do
-      case VirtualCrypto.Money.create(guild: int_guild_id, name: options["name"], unit: options["unit"]) do
-        {:ok} -> {:ok, "通貨の作成に成功しました！ `/info " <> options["unit"] <> "`コマンドで通貨の情報をご覧ください。"}
-        {:error, :guild} -> {:error, "このギルドではすでに通貨が作成されています。"}
-        {:error, :unit} -> {:error, options["unit"] <> "という単位の通貨は存在しています。別の名前を使用してください。"}
-        {:error, :name} -> {:error, options["name"] <> "という名前の通貨は存在しています。別の名前を使用してください。"}
-        _ -> {:error, "不明なエラーが発生しました。時間を開けてもう一度実行してください。"}
+    {int_guild_id, _} = Integer.parse guild_id
+    {int_permissions, _} = Integer.parse params["member"]["permissions"]
+    if Discord.Permissions.check(int_permissions, Discord.Permissions.administrator()) do
+      if name_unit_check(options["name"], options["unit"]) do
+        case VirtualCrypto.Money.create(guild: int_guild_id, name: options["name"], unit: options["unit"]) do
+          {:ok} -> {:ok, "通貨の作成に成功しました！ `/info " <> options["unit"] <> "`コマンドで通貨の情報をご覧ください。"}
+          {:error, :guild} -> {:error, "このギルドではすでに通貨が作成されています。"}
+          {:error, :unit} -> {:error, options["unit"] <> "という単位の通貨は存在しています。別の名前を使用してください。"}
+          {:error, :name} -> {:error, options["name"] <> "という名前の通貨は存在しています。別の名前を使用してください。"}
+          _ -> {:error, "不明なエラーが発生しました。時間を開けてもう一度実行してください。"}
+        end
+      else
+        {:error, "通貨の名前は2から32文字以内の英数字、単位は1から10文字以内の英数字を使ってください。"}
       end
     else
-      {:error, "通貨の名前は2から32文字以内の英数字、単位は1から10文字以内の英数字を使ってください。"}
+      {:error, "実行には管理者権限が必要です。"}
     end
   end
 
