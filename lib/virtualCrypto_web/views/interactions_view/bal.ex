1475321c469586d7306495502cddd0a9b33ea79f
tignear <tignear+m@gmail.com>
2020-12-27 18:18:32 +0900

    implement bal command

---
+++ lib/virtualCrypto_web/views/interactions_view/bal.ex
@@ -0,0 +1,18 @@
+defmodule VirtualCryptoWeb.InteractionsView.Bal do
+  def render_line(%{amount: amount, name: name, unit: unit}),do: name <> ": " <> Integer.to_string(amount)<>unit
+  def render_content([]) do
+    "所持通貨一覧\n```\n通貨を持っていません。\n```"
+  end
+  def render_content(params) do
+    "所持通貨一覧\n```yaml\n"<>(params|>Enum.map(&render_line/1)|>Enum.join("\n"))<>"\n```"
+  end
+  def render(params) do
+    %{
+      type: 3,
+      data: %{
+        flags: 64,
+        content: render_content(params),
+      }
+    }
+  end
+end
