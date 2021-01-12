cfa450f4d1d0fda4817a8c91c2116cc43d0edfa4
tignear <tignear+m@gmail.com>
2021-01-12 20:32:06 +0900

    add get_guild_member_with_status_code and roles to api accessor

--- lib/discord/api/v8/v8.ex
+++ lib/discord/api/v8/v8.ex
@@ -2,9 +2,10 @@ defmodule Discord.Api.V8 do
   @base_url "https://discord.com/api/v8/"
   @base_headers [
     {"Authorization", "Bot " <> Application.get_env(:virtualCrypto, :bot_token)},
-    {"Content-Type", "application/json"}]
+    {"Content-Type", "application/json"}
+  ]
 
-  defp make_params params do
+  defp make_params(params) do
     "?" <> (params |> Enum.map(fn {name, value} -> name <> "=" <> value end) |> Enum.join("&"))
   end
 
@@ -21,12 +22,24 @@ defmodule Discord.Api.V8 do
   end
 
   def get_guild_members(guild_id, limit \\ 1000) do
-    {:ok, response} = get(["guilds", to_string(guild_id), "members"], [{"limit", to_string(limit)}])
+    {:ok, response} =
+      get(["guilds", to_string(guild_id), "members"], [{"limit", to_string(limit)}])
+
     Jason.decode!(response.body)
   end
 
+  def get_guild_member_with_status_code(guild_id, user_id) do
+    {:ok, response} = get(["guilds", to_string(guild_id), "members", to_string(user_id)], [])
+    {response.status_code, Jason.decode!(response.body)}
+  end
+  def roles(guild_id) do
+    {:ok, response} = get(["guilds", to_string(guild_id), "roles"], [])
+    Jason.decode!(response.body)
+  end
   def get_guild(guild_id, with_counts \\ false) do
-    {:ok, response} = get(["guilds", to_string(guild_id)], [{"with_counts", to_string(with_counts)}])
+    {:ok, response} =
+      get(["guilds", to_string(guild_id)], [{"with_counts", to_string(with_counts)}])
+
     Jason.decode!(response.body)
   end
 end
