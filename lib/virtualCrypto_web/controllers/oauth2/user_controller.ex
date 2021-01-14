7fc507a249c5ef735b8d396e8cc19f29b25f5128
tignear <tignear+m@gmail.com>
2021-01-15 01:23:37 +0900

    Revert "move"
    This reverts commit b7a7b45ff16d92decb031b919a0eaebc56ed9c02.

--- lib/virtualCrypto_web/controllers/oauth2/user_controller.ex
+++ lib/virtualCrypto_web/controllers/oauth2/user_controller.ex
@@ -2,7 +2,7 @@ defmodule VirtualCryptoWeb.V1.UserController do
   use VirtualCryptoWeb, :controller
   import Plug.Conn, only: [get_session: 2, halt: 1]
 
-  def me(conn, _params) do
+  def me(conn, params) do
     user = get_session(conn, :user)
     render(
       conn,
