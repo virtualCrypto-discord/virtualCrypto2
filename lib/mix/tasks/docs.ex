7a586c74c179662e625c5b5f2d028c40868e4068
sizumita <contact@sumidora.com>
2021-01-18 09:59:44 +0900

    add parse_all function

--- lib/mix/tasks/docs.ex
+++ lib/mix/tasks/docs.ex
@@ -4,10 +4,6 @@ defmodule Mix.Tasks.Docs.Parse do
   def run(_) do
     Mix.Task.run("app.start")
 
-    {:ok,  files} = File.ls "./docs"
-    files
-    |> Enum.filter(fn file -> String.ends_with?(file, ".md") end)
-    |> Enum.map(fn file -> VirtualCrypto.MarkdownParser.parse(file) end)
-
+    VirtualCrypto.MarkdownParser.parse_all()
   end
 end
