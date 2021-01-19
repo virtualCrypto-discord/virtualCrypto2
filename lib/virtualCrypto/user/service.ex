a0703e6d4828d4b677f1d36ca2c9617b9a3cd52b
tignear <tignear+m@gmail.com>
2021-01-19 20:38:42 +0900

    fix bug at insert_user_if_not_exists

--- lib/virtualCrypto/user/service.ex
+++ lib/virtualCrypto/user/service.ex
@@ -1,13 +1,13 @@
 defmodule VirtualCrypto.User do
   alias VirtualCrypto.Repo
 
-  def insert_user_if_not_exits(discord_id) do
+  def insert_user_if_not_exists(discord_id) do
     with {:ok, nil} <- {:ok, Repo.get_by(VirtualCrypto.User.User, discord_id: discord_id)},
          {:ok, %VirtualCrypto.User.User{id: nil}} <-
            Repo.insert(%VirtualCrypto.User.User{discord_id: discord_id, status: 0},
              on_conflict: :nothing
            ) do
-      Repo.get_by(VirtualCrypto.User.User, discord_id: discord_id)
+      {:ok, Repo.get_by(VirtualCrypto.User.User, discord_id: discord_id)}
     end
   end
 
