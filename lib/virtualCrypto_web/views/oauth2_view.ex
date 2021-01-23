4d0e4dfbeb18be556e00a19da1986dff4b75d71e
tignear <tignear+m@gmail.com>
2021-01-23 19:10:06 +0900

    implement endpoints

--- lib/virtualCrypto_web/views/oauth2_view.ex
+++ lib/virtualCrypto_web/views/oauth2_view.ex
@@ -66,4 +66,62 @@ defmodule VirtualCryptoWeb.Oauth2View do
       "error" => to_string(v)
     }
   end
+
+  def render("ok.register.json", %{
+        application: application,
+        registration_access_token: registration_access_token,
+        registration_client_uri: registration_client_uri
+      }) do
+    %{
+      "client_id" => application.client_id,
+      "client_secret" => application.client_secret,
+      "registration_access_token" => registration_access_token,
+      "registration_client_uri" => registration_client_uri
+    }
+  end
+
+  def render("error.register.json", %{error: error, error_description: error_description}) do
+    %{
+      "error" => to_string(error),
+      "error_description" => to_string(error_description)
+    }
+  end
+
+  def render("info.register.json", %{
+        application: application,
+        redirect_uris: redirect_uris,
+        user: user
+      }) do
+    %{
+      "client_id" => application.client_id,
+      "client_secret" => application.client_secret,
+      "redirect_uris" => redirect_uris |> Enum.map(& &1.redirect_uri),
+      "user_id" => user.id,
+      "discord_user_id" =>
+        unless user.discord_id == nil do
+          to_string(user.discord_id)
+        else
+          nil
+        end,
+      "application_type" => application.application_type,
+      "client_name" => application.client_name,
+      "client_uri" => application.client_uri,
+      "discord_support_server_invite_slug" => application.discord_support_server_invite_slug,
+      "grant_types" => application.grant_types,
+      "logo_uri" => application.logo_uri,
+      "owner_discord_id" => to_string(application.owner_discord_id),
+      "response_types" => application.response_types
+    }
+  end
+
+  def render("clients.register.json", %{applications: applications}) do
+    Enum.group_by(applications, fn {application, _, _} -> application.id end)
+    |> Enum.map(fn {_application_id, [{application, user, _redirect_uri} | _tail] = list} ->
+      render("info.register.json", %{
+        application: application,
+        user: user,
+        redirect_uris: list |> Enum.map(&elem(&1, 2)) |> Enum.filter(&(&1 != nil))
+      })
+    end)
+  end
 end
