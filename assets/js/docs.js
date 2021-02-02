dc04b34355d164e5de2a2fe3e0473feeeaaf8d61
Sumito Izumita <sumito@izumita.com>
2021-02-02 12:04:17 +0900

    remove documents

--- assets/js/docs.js
+++
@@ -1,7 +0,0 @@
-import { Elm } from "../elm/src/Docs.elm"
-const element = document.getElementById("elm-docs")
-
-Elm.Docs.init({
-    node: element,
-    flags: element.dataset.raw
-})
--- lib/virtualCrypto_web/controllers/document_controller.ex
+++ lib/virtualCrypto_web/controllers/document_controller.ex
@@ -1,17 +1,18 @@
 defmodule VirtualCryptoWeb.DocumentController do
   use VirtualCryptoWeb, :controller
+  @base "https://github.com/virtualCrypto-discord/virtualcrypto-docs/tree/master/docs/"
 
   def index(conn, _) do
     render(conn, "index.html")
   end
 
   def commands(conn, _) do
-    {:ok, md} = File.read("./docs/parsed/commands.md.html")
-    render(conn, "docs.html", md: md)
+    conn
+    |> redirect(external: @base <> "Commands.md")
   end
 
   def about(conn, _) do
-    {:ok, md} = File.read("./docs/parsed/about.md.html")
-    render(conn, "docs.html", md: md)
+    conn
+    |> redirect(external: @base <> "About.md")
   end
 end
