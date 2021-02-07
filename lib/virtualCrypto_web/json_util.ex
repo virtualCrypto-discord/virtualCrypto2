4c9dd77e117d7448773b443a01a9ce486b9114a0
tignear <tignear+m@gmail.com>
2021-02-07 14:38:31 +0900

    fix

--- lib/virtualCrypto_web/json_util.ex
+++ lib/virtualCrypto_web/json_util.ex
@@ -1,19 +1,26 @@
 defmodule VirtualCryptoWeb.JsonUtil do
-  defp is_safe_integer(v) do
-  end
-
+  @max_safe_integer 9007199254740991
+  defguard is_safe_integer(v) when is_integer(v) and -@max_safe_integer <= v and v <= @max_safe_integer
   def to_integer(v) when is_binary(v) do
     String.to_integer(v)
   end
 
-  def to_integer(v) when is_integer(v) do
+  def to_integer(v) when is_safe_integer(v) do
     v
   end
-
+  def to_integer(_) do
+    :error
+  end
   def parse_to_integer(v) when is_binary(v) do
     case Integer.parse(v) do
       {v, ""} -> v
       _ -> :error
     end
+  end
+  def parse_to_integer(v) when is_safe_integer(v) do
+    v
+  end
+  def parse_to_integer(_) do
+    :error
   end
 end
