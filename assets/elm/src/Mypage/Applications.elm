db526a880dd011760579253e73b1d19aeff51bff
Sumito Izumita <sumito@izumita.com>
2021-02-24 18:04:20 +0900

    add api call

--- assets/elm/src/Mypage/Applications.elm
+++ assets/elm/src/Mypage/Applications.elm
@@ -1,21 +1,57 @@
 module Mypage.Applications exposing (..)
 import Html exposing (..)
+import Url.Builder exposing (absolute)
+import Http
+import Api
+import Json.Decode exposing (field, Decoder, map2, map4, string, int, list)
 
 
-type alias Model = {}
+type alias Model =
+    { applications : Maybe Applications
+    }
+
+type alias Application =
+    { client_id : String
+    , client_name : String
+    , user_id : String
+    , client_secret : String
+    }
+
+type alias Applications = List Application
+
+applicationDecoder : Decoder Application
+applicationDecoder =
+    map4 Application
+        (field "client_id" string)
+        (field "client_name" string)
+        (field "user_id" string)
+        (field "client_secret" string)
+
+applicationsDecoder : Decoder Applications
+applicationsDecoder =
+    applicationDecoder |> list
+
+getApplications : String -> Cmd Msg
+getApplications token =
+    Api.get
+            { url = absolute [ "api", "v1", "users", "@me", "claims" ] []
+            , expect = Http.expectJson GotApplications applicationsDecoder
+            , token = token
+            }
 
 
 type Msg
-    = None
+    = GotApplications (Result Http.Error Applications)
 
 
 initCmd : String -> Cmd Msg
-initCmd accessToken = Cmd.none
+initCmd accessToken =
+    getApplications accessToken
 
 
 initModel : String -> Model
 initModel _ =
-    {}
+    {applications = Maybe.Nothing}
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
