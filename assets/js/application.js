ddca6f19b2fff29627a9e4aa3450c630fe2ce42b
Sumito Izumita <sumito@izumita.com>
2021-03-04 17:14:57 +0900

    add copy button

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,7 +1,12 @@
 import { Elm } from "../elm/src/Application.elm"
 
 const element = document.getElementById("elm-app")
-Elm.Application.init({
+const app = Elm.Application.init({
     node: element,
     flags: JSON.parse(element.dataset.json)
+})
+
+app.ports.copy.subscribe(id => {
+    document.querySelector(id).select();
+    document.execCommand('copy');
 })
