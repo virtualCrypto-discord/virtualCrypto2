58747ad803620cbb1b26b74102080613c3d71067
tignear <tignear+m@gmail.com>
2021-04-11 15:08:26 +0900

    add test for /claim make from guild

--- test/virtualCrypto_web/controllers/api/interactions/claim_make_test.exs
+++ test/virtualCrypto_web/controllers/api/interactions/claim_make_test.exs
@@ -1,11 +1,44 @@
 defmodule InteractionsControllerTest.Claim.Make do
   use VirtualCryptoWeb.InteractionsCase, async: true
+  setup :setup_claim
 
-  test "make valid claim", %{conn: conn} do
+  test "valid request from guild", %{conn: conn, user1: user1,user2: user2, unit: unit} do
     conn =
-      post(
+      post_command(
         conn,
-        Routes.interactions_path(conn, :index)
+        %{
+          type: 2,
+          data: %{
+            name: "claim",
+            options: [
+              %{
+                name: "make",
+                options: [
+                  %{
+                    name: "user",
+                    value: to_string(user1)
+                  },
+                  %{
+                    name: "unit",
+                    value: unit
+                  },
+                  %{
+                    name: "amount",
+                    value: 100
+                  }
+                ]
+              }
+            ]
+          },
+          member: %{
+            user: %{
+              id: to_string(user2)
+            }
+          }
+        }
       )
+
+    assert %{"data" => %{"content" => content, "flags" => 64},"type" => 4} = json_response(conn, 200)
+    assert Regex.match?(~r/請求id: \d+ で請求を受け付けました。`\/claim list`でご確認ください。/,content)
   end
 end
