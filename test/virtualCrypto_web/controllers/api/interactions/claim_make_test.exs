77a768516ea53d809f3b13f3162a32f944b8e7b1
tignear <tignear+m@gmail.com>
2021-04-11 15:40:46 +0900

    test in detail

--- test/virtualCrypto_web/controllers/api/interactions/claim_make_test.exs
+++ test/virtualCrypto_web/controllers/api/interactions/claim_make_test.exs
@@ -2,7 +2,7 @@ defmodule InteractionsControllerTest.Claim.Make do
   use VirtualCryptoWeb.InteractionsCase, async: true
   setup :setup_claim
 
-  test "valid request from guild", %{conn: conn, user1: user1,user2: user2, unit: unit} do
+  test "valid request from guild", %{conn: conn, user1: user1, user2: user2, unit: unit} do
     conn =
       post_command(
         conn,
@@ -38,7 +38,13 @@ defmodule InteractionsControllerTest.Claim.Make do
         }
       )
 
-    assert %{"data" => %{"content" => content, "flags" => 64},"type" => 4} = json_response(conn, 200)
-    assert Regex.match?(~r/請求id: \d+ で請求を受け付けました。`\/claim list`でご確認ください。/,content)
+    assert %{"data" => %{"content" => content, "flags" => 64}, "type" => 4} =
+             json_response(conn, 200)
+
+    regex = ~r/請求id: (\d+) で請求を受け付けました。`\/claim list`でご確認ください。/
+    assert [_, claim_id] = Regex.run(regex, content)
+
+    assert {%{amount: 100}, %{unit: ^unit}, %{discord_id: ^user2}, %{discord_id: ^user1}} =
+             VirtualCrypto.Money.get_claim_by_id(claim_id)
   end
 end
