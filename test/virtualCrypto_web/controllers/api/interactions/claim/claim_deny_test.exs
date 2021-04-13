4610f02315308a3f7223a401b1f6292bafb7324c
tignear <tignear+m@gmail.com>
2021-04-14 00:56:55 +0900

    add more test case

--- test/virtualCrypto_web/controllers/api/interactions/claim/claim_deny_test.exs
+++ test/virtualCrypto_web/controllers/api/interactions/claim/claim_deny_test.exs
@@ -4,32 +4,6 @@ defmodule InteractionsControllerTest.Claim.Deny do
   import InteractionsControllerTest.Claim.Patch
   setup :setup_claim
 
-  test "deny pending claim by claimant", %{conn: conn, claims: claims, user1: user1} do
-    conn =
-      post_command(
-        conn,
-        patch_from_guild("deny", (claims |> at(0) |> elem(0)).id, user1)
-      )
-
-    assert %{
-             "data" => %{"content" => "エラー: この請求に対してこの操作を行う権限がありません。", "flags" => 64},
-             "type" => 4
-           } = json_response(conn, 200)
-  end
-
-  test "deny pending claim by not related user", %{conn: conn, claims: claims} do
-    conn =
-      post_command(
-        conn,
-        patch_from_guild("deny", (claims |> at(0) |> elem(0)).id, -1)
-      )
-
-    assert %{
-             "data" => %{"content" => "エラー: この請求に対してこの操作を行う権限がありません。", "flags" => 64},
-             "type" => 4
-           } = json_response(conn, 200)
-  end
-
   test "deny pending claim by payer", %{conn: conn, claims: claims, user2: user2} do
     claim_id = (claims |> at(0) |> elem(0)).id
     claim_id_str = to_string(claim_id)
@@ -49,54 +23,56 @@ defmodule InteractionsControllerTest.Claim.Deny do
     assert [_, ^claim_id_str] = Regex.run(regex, content)
   end
 
-  test "deny approved claim",
-       %{conn: conn, claims: claims, user2: user2} do
-    conn =
-      post_command(
-        conn,
-        patch_from_guild("deny", (claims |> approved_claim() |> elem(0)).id, user2)
-      )
-
-    assert %{
-             "data" => %{
-               "content" => "エラー: この請求に対してこの操作を行うことは出来ません。",
-               "flags" => 64
-             },
-             "type" => 4
-           } = json_response(conn, 200)
+  test "deny pending claim by claimant", %{conn: conn, claims: claims, user1: user1} do
+    test_invalid_operator(conn, "deny", claims |> at(0) |> elem(0), user1)
   end
 
-  test "deny denied claim",
-       %{conn: conn, claims: claims, user2: user2} do
-    conn =
-      post_command(
-        conn,
-        patch_from_guild("deny", (claims |> denied_claim() |> elem(0)).id, user2)
-      )
-
-    assert %{
-             "data" => %{
-               "content" => "エラー: この請求に対してこの操作を行うことは出来ません。",
-               "flags" => 64
-             },
-             "type" => 4
-           } = json_response(conn, 200)
+  test "deny pending claim by not related user", %{conn: conn, claims: claims} do
+    test_invalid_operator(conn, "deny", claims |> at(0) |> elem(0), -1)
   end
 
-  test "deny canceled claim",
+  test "deny approved claim by payer",
        %{conn: conn, claims: claims, user2: user2} do
-    conn =
-      post_command(
-        conn,
-        patch_from_guild("deny", (claims |> canceled_claim() |> elem(0)).id, user2)
-      )
+    test_invalid_status(conn, "deny", claims |> approved_claim() |> elem(0), user2)
+  end
 
-    assert %{
-             "data" => %{
-               "content" => "エラー: この請求に対してこの操作を行うことは出来ません。",
-               "flags" => 64
-             },
-             "type" => 4
-           } = json_response(conn, 200)
+  test "deny approved claim by claimant",
+       %{conn: conn, claims: claims, user1: user1} do
+    test_invalid_operator(conn, "deny", claims |> approved_claim() |> elem(0), user1)
+  end
+
+  test "deny approved claim by not related user",
+       %{conn: conn, claims: claims} do
+    test_invalid_operator(conn, "deny", claims |> approved_claim() |> elem(0), -1)
+  end
+
+  test "deny denied claim by payer",
+       %{conn: conn, claims: claims, user2: user2} do
+    test_invalid_status(conn, "deny", claims |> denied_claim() |> elem(0), user2)
+  end
+
+  test "deny denied claim by claimant",
+       %{conn: conn, claims: claims, user1: user1} do
+    test_invalid_operator(conn, "deny", claims |> denied_claim() |> elem(0), user1)
+  end
+
+  test "deny denied claim by not related user",
+       %{conn: conn, claims: claims} do
+    test_invalid_operator(conn, "deny", claims |> denied_claim() |> elem(0), -1)
+  end
+
+  test "deny canceled claim by payer",
+       %{conn: conn, claims: claims, user2: user2} do
+    test_invalid_status(conn, "deny", claims |> canceled_claim() |> elem(0), user2)
+  end
+
+  test "deny canceled claim by claimant",
+       %{conn: conn, claims: claims, user1: user1} do
+    test_invalid_operator(conn, "deny", claims |> canceled_claim() |> elem(0), user1)
+  end
+
+  test "deny canceled claim by not related user",
+       %{conn: conn, claims: claims} do
+    test_invalid_operator(conn, "deny", claims |> canceled_claim() |> elem(0), -1)
   end
 end
