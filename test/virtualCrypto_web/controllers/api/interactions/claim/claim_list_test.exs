f78c1bbf31b2905f022f6d24bdab40b2f33acdd8
tignear <tignear+m@gmail.com>
2021-04-18 01:03:04 +0900

    fix test

--- test/virtualCrypto_web/controllers/api/interactions/claim/claim_list_test.exs
+++ test/virtualCrypto_web/controllers/api/interactions/claim/claim_list_test.exs
@@ -38,7 +38,6 @@ defmodule InteractionsControllerTest.Claim.List do
              "type" => 4
            } = json_response(conn, 200)
 
-    IO.inspect(content)
     regex = ~r/友達への請求:\n(.*)\n\n自分に来た請求:\n(.*)/us
     assert [_, outgoing, incoming] = Regex.run(regex, content)
     claims = VirtualCrypto.Money.get_claims(VirtualCrypto.Money.DiscordService, user1, "pending")
@@ -49,6 +48,7 @@ defmodule InteractionsControllerTest.Claim.List do
         {_claim, _money, %{discord_id: ^user1}, _payer} -> true
         _ -> false
       end)
+      |> Enum.sort(&(elem(&1, 0).id <= elem(&2, 0).id))
       |> Enum.map(&generate_outgoing_claim_text(&1))
       |> Enum.join("\n")
 
@@ -58,6 +58,7 @@ defmodule InteractionsControllerTest.Claim.List do
         {_claim, _money, _claimant, %{discord_id: ^user1}} -> true
         _ -> false
       end)
+      |> Enum.sort(&(elem(&1, 0).id <= elem(&2, 0).id))
       |> Enum.map(&generate_incoming_claim_text(&1))
       |> Enum.join("\n")
 
