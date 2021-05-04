c6a43dba9b787ef828c2871d2c2ac919f62b3a31
tignear <tignear+m@gmail.com>
2021-05-04 16:57:07 +0900

    testing that Idempotency-Key is independent for each user

--- test/virtualCrypto_web/controllers/api/v2/user_transactions/idempotency_test.exs
+++ test/virtualCrypto_web/controllers/api/v2/user_transactions/idempotency_test.exs
@@ -1,7 +1,7 @@
 defmodule UserTransactionControllerTest.Pay.Idempotency.Single do
   use VirtualCryptoWeb.RestCase, async: true
-  import Plug.Conn
   import InteractionsControllerTest.Pay.Helper
+
   setup :setup_money
 
   defp exec(conn, json, idempotency_key \\ "1dEmP0104Ke1") do
@@ -21,7 +21,7 @@ defmodule UserTransactionControllerTest.Pay.Idempotency.Single do
            }
   end
 
-  test "duplicate valid request", %{conn: conn} = ctx do
+  test "prevent duplicate request", %{conn: conn} = ctx do
     conn = set_user_auth(conn, :user, ctx.user1, ["vc.pay"])
     conn2 = set_user_auth(build_rest_conn(), :user, ctx.user1, ["vc.pay"])
     b1 = get_amount(ctx.user1, ctx.currency)
@@ -52,6 +52,44 @@ defmodule UserTransactionControllerTest.Pay.Idempotency.Single do
 
     assert idempotency_ok?(conn) != idempotency_ok?(conn2)
     assert idempotency_duplicate?(conn2) != idempotency_duplicate?(conn)
+
+    assert get_amount(ctx.user1, ctx.currency) == b1 - 20
+
+    assert get_amount(ctx.user2, ctx.currency) == b2 + 20
+  end
+
+  test "Idempotency-Key is independent for each user", ctx do
+    conn = set_user_auth(ctx.conn, :user, ctx.user1, ["vc.pay"])
+    conn2 = set_user_auth(build_rest_conn(), :user, ctx.user2, ["vc.pay"])
+
+    b1 = get_amount(ctx.user1, ctx.currency)
+    b2 = get_amount(ctx.user2, ctx.currency)
+
+    conn =
+      exec(
+        conn,
+        %{
+          unit: ctx.unit,
+          receiver_discord_id: to_string(ctx.user2),
+          amount: to_string(40)
+        }
+      )
+
+    conn2 =
+      exec(
+        conn2,
+        %{
+          unit: ctx.unit,
+          receiver_discord_id: to_string(ctx.user1),
+          amount: to_string(20)
+        }
+      )
+
+    assert response(conn, 201)
+    assert response(conn2, 201)
+
+    assert idempotency_ok?(conn)
+    assert idempotency_ok?(conn2)
 
     assert get_amount(ctx.user1, ctx.currency) == b1 - 20
 
