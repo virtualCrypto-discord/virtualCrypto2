a270bce566860d14478043feba4e77b7135a02b5
tignear <tignear+m@gmail.com>
2021-05-29 18:01:04 +0900

    filtering by status

--- lib/virtualCrypto_web/interactions/button.ex
+++ lib/virtualCrypto_web/interactions/button.ex
@@ -1,26 +1,41 @@
 defmodule VirtualCryptoWeb.Interaction.Button do
   def handle(
         ["claim", "list", "last"],
-        _query,
+        query,
         %{
           "member" => %{"user" => user}
         },
         _conn
       ) do
-    {a, b, params} = VirtualCryptoWeb.Interaction.Claim.List.last(user)
+    {a, b, params} =
+      VirtualCryptoWeb.Interaction.Claim.List.last(
+        user,
+        query
+        |> Map.new()
+        |> Map.get("flags", "0")
+        |> VirtualCryptoWeb.Interaction.Claim.List.decode_options()
+      )
 
     {"claim", {a, b, params |> Map.put(:type, :button)}}
   end
 
   def handle(
         ["claim", "list", n],
-        _query,
+        query,
         %{
           "member" => %{"user" => user}
         },
         _conn
       ) do
-    {a, b, params} = VirtualCryptoWeb.Interaction.Claim.List.page(user, n |> String.to_integer())
+    {a, b, params} =
+      VirtualCryptoWeb.Interaction.Claim.List.page(
+        user,
+        n |> String.to_integer(),
+        query
+        |> Map.new()
+        |> Map.get("flags", "0")
+        |> VirtualCryptoWeb.Interaction.Claim.List.decode_options()
+      )
 
     {"claim", {a, b, params |> Map.put(:type, :button)}}
   end
