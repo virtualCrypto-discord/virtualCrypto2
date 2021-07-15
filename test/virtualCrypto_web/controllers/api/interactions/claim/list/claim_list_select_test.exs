a48d9ee8468ff1e9c1b7a5f714b16a888ef69c90
tignear <tignear+m@gmail.com>
2021-07-16 00:56:19 +0900

    progress of test

--- test/virtualCrypto_web/controllers/api/interactions/claim/list/claim_list_select_test.exs
+++ test/virtualCrypto_web/controllers/api/interactions/claim/list/claim_list_select_test.exs
@@ -1,9 +1,49 @@
 defmodule InteractionsControllerTest.Claim.List.Select do
   use VirtualCryptoWeb.InteractionsCase, async: true
-  import InteractionsControllerTest.Claim.Helper
+  import InteractionsControllerTest.Helper.Common
   alias VirtualCryptoWeb.Interaction.CustomId
   alias VirtualCryptoWeb.Interaction.CustomId.UI.Button
   alias VirtualCryptoWeb.Interaction.CustomId.UI.SelectMenu
   alias VirtualCryptoWeb.Interaction.Claim.List.Options, as: ListOptions
   alias VirtualCryptoWeb.Interaction.Claim.List.Helper
+  alias VirtualCrypto.Exterior.User.Discord, as: DiscordUser
+  setup :setup_claim
+
+  test "selection", %{conn: conn, user1: user1} do
+    claims =
+      VirtualCrypto.Money.get_claims(
+        %DiscordUser{id: user1},
+        ["pending"],
+        :all,
+        nil,
+        :desc_claim_id,
+        %{page: 1},
+        5
+      )
+
+    custom_id_select =
+      CustomId.encode(
+        SelectMenu.claim_select() <>
+          ListOptions.encode(%ListOptions{
+            approved: false,
+            canceled: false,
+            denied: false,
+            pending: true,
+            page: 1,
+            position: :all,
+            related_user: 0
+          }) <> Helper.encode_claim_ids(claims.claims)
+      )
+
+    conn =
+      execute_interaction(
+        conn,
+        select_from_guild(%{
+          custom_id: custom_id_select,
+          values: claims.claims |> Enum.map(& to_string(&1.claim.id))
+        },user1)
+      )
+
+    IO.inspect(json_response(conn, 200))
+  end
 end
