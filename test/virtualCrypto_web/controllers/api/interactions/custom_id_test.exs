b8041f5774ae8ab8e839e40c030c71fbec79280e
tignear <tignear+m@gmail.com>
2021-07-19 19:16:44 +0900

    improve custom_id test

--- test/virtualCrypto_web/controllers/api/interactions/custom_id_test.exs
+++ test/virtualCrypto_web/controllers/api/interactions/custom_id_test.exs
@@ -1,11 +1,11 @@
 defmodule VirtualCryptoWeb.CustomIdTest do
   use ExUnit.Case, async: true
-
+  alias VirtualCryptoWeb.Interaction.CustomId
   test "custom_id" do
     for a <- 0..255, b <- 0..255, c <- 0..127 do
-      VirtualCryptoWeb.Interaction.CustomId.encode(<<a, b, c>>)
+      assert <<a,b,c,0,0>> == CustomId.parse(CustomId.encode(<<a, b, c>>))
     end
 
-    VirtualCryptoWeb.Interaction.CustomId.encode(<<255, 255, 255, 255, 255>>)
+    assert <<255,255,255,255,255>> == CustomId.encode(<<255, 255, 255, 255, 255>>)
   end
 end
