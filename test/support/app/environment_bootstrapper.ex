80df4f4cfaf1792a2666cd0ddf6a8b2b8e76f592
tignear <tignear+m@gmail.com>
2025-09-08 00:35:56 +0900

    refactor(environment): restructure contractor data format in setup_money function

--- test/support/app/environment_bootstrapper.ex
+++ test/support/app/environment_bootstrapper.ex
@@ -191,12 +191,17 @@ defmodule VirtualCrypto.EnvironmentBootstrapper do
 
     {:ok, contract1} =
       VirtualCrypto.Money.create_contract(d.app1_app, %{
-        contractors: [%{discord_id: d.user1, deposits: [
+        contractors: [
           %{
-            deposit_amount: 100,
-            currency_unit: d.unit
+            discord_id: d.user1,
+            deposits: [
+              %{
+                deposit_amount: 100,
+                currency_unit: d.unit
+              }
+            ]
           }
-        ]}]
+        ]
       })
 
     Map.put(d, :contracts, %{contract1: contract1})
