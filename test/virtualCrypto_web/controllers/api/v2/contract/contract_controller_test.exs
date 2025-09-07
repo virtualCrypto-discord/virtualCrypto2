225584c0f2df96808a0276843e36e6262650fbd5
tignear <tignear+m@gmail.com>
2025-09-07 23:53:54 +0900

    test(contract): update contract creation test to assert successful response instead of error

--- test/virtualCrypto_web/controllers/api/v2/contract/contract_controller_test.exs
+++ test/virtualCrypto_web/controllers/api/v2/contract/contract_controller_test.exs
@@ -63,10 +63,21 @@ defmodule ContractControllerTest.V2 do
         Routes.v2_contract_path(conn, :post),
         valid_contract_request(user1)
       )
-
-    assert json_response(conn, 403) == %{
-             "error" => "invalid_token",
-             "error_description" => "permission_denied"
-           }
+      user1 = to_string(user1)
+    assert %{
+      "contractor" => [
+        %{
+          "user" => %{
+            "id" => _,
+            "discord" => %{
+              "id" => ^user1
+            }
+          },
+          "deposits" => []
+        }
+      ],
+      "created_at" => _,
+      "updated_at" => _,
+    } = json_response(conn, 201)
   end
 end
